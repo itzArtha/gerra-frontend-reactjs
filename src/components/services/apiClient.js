90ae1de4c39ca9c8b782eabcd46150cfbc0df9b3
arthaputra <manager.arthachan@gmail.com>
2022-06-25 09:04:39 +0800

    api client

--- src/components/services/apiClient.js
+++ src/components/services/apiClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  // baseURL: "http://localhost:8000",
-  baseURL: "https://api.exotix.id",
+  baseURL: "http://localhost:8000",
+  // baseURL: "https://api.tokoevent.id",
   withCredentials: true,
 });
 
